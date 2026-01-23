3ce187546b8cc48cd9568e40c0a58e5b90af6847
Pavel Rabekin <pavelrabekin@gmail.com>
2026-01-23 13:50:10 +0300

    refactor: move code snippet to 'commands' folder

---
+++ bin/commands/add.js
@@ -0,0 +1,22 @@
+const { program } = require('commander');
+const addExpense = require('../actions/add');
+
+function commandAdd() {
+  program
+    .name('expense-tracker')
+    .description('A simple CLI to track your expenses')
+    .version('1.0.0');
+
+  program
+    .command('add')
+    .description('Add a new expense')
+    .requiredOption('-D, --description <string>', 'description of the expense')
+    .requiredOption(
+      '-A, --amount <number>',
+      'amount of the expense',
+      parseFloat
+    )
+    .action(addExpense);
+}
+
+module.exports = commandAdd;
--- bin/index.js
+++ bin/index.js
@@ -1,22 +1,8 @@
 const { program } = require('commander');
-const addExpense = require('./actions/add');
+const commandAdd = require('./commands/add');
 
 function main() {
-  program
-    .name('expense-tracker')
-    .description('A simple CLI to track your expenses')
-    .version('1.0.0');
-
-  program
-    .command('add')
-    .description('Add a new expense')
-    .requiredOption('-D, --description <string>', 'description of the expense')
-    .requiredOption(
-      '-A, --amount <number>',
-      'amount of the expense',
-      parseFloat
-    )
-    .action(addExpense);
+  commandAdd();
 
   program.parse();
 }
